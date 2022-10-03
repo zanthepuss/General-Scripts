4f37d0029f5f363ce0ad52e11e3b1f10917c7fae
Liam <31197356+zanthepuss@users.noreply.github.com>
2022-10-03 11:02:47 -0600

    Add files via upload

---
+++ CTempCheck.ps1
@@ -0,0 +1,15 @@
+﻿clear-host 
+
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force  
+
+$directoyPath="C:\Temp";
+if(!(Test-Path -path $directoyPath))  
+{  
+    New-Item -ItemType directory -Path $directoyPath
+   # Write-Host "Folder path has been created successfully at: " $directoyPath
+               
+}
+else
+{
+ # Write-Host "The given folder path $directoyPath already exists";
+}
